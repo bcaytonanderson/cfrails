f09cda915e2f93a7d28f00b88d48487d5055c293
Bradley Anderson <bcaytonanderson@gmail.com>
2015-09-16 15:49:35 -0500

    Small bug in products controller- unexpected end of input error. Fixed. Also, seeded database with products and put images in the assets folder.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.create([{name: 'Empty Box', description: 'Send someone special this big box. Their high hopes will be dashed when they open it to find NOTHING!', image_url: '../app/assets/images/emptybox.jpg', colour: 'brown'},
+                {name: 'Blank Letter', description: 'They haven\'t heard from you in a while, and with this blank letter addressed to them it\'ll be even longer!', image_url: '../app/assets/images/blankletter.jpg', colour: 'white'},
+                {name: '$0 Gift Card', description: 'Send someone a gift card to their favorite store, and imagine their disappointment when they try to use it!', image_url: '../app/assets/images/giftcard.jpg', colour: 'white'},
+                {name: 'Chocolateless Box', description: 'A box of chocolates is a wonderful treat, and this empty box will be sure to entice a craving and promptly disappoint.', image_url: '../app/assets/images/chocolate.jpg', colour: 'red'}])
