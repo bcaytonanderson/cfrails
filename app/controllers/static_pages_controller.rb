8dff135ca0b26b406416359f4895030288a455eb
Bradley Anderson <bcaytonanderson@gmail.com>
2015-10-02 17:48:04 -0500

    Lesson 38, mailers. Created contact page with form, created action in static_pages_controller, created thank_you view, added SendGrid to app with Heroku, edited environment.rb. Also tweated CSS: added red border to all elements. Got the products search bar to disappear at smallest grid size using .hidden-xs bootstrap class.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,7 +6,15 @@ class StaticPagesController < ApplicationController
     @products = Product.limit(3)
   end
 
-
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    ActionMailer::Base.mail(:from => @email,
+          :to       => 'bcaytonanderson@gmail.com',
+          :subject  => "DisDepot: Contact form message from #{@name}",
+          :body     => @message).deliver
+  end
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
 
   get 'static_pages/index'
 
-
+  post 'static_pages/thank_you'
 
 
 end
