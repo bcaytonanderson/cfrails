27722f9fc8e2ab97d570cce30b0bcbee85463c24
Bradley Anderson <bcaytonanderson@gmail.com>
2017-10-07 17:39:01 -0500

    5.9, edited products controller for will_paginate.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -18,7 +18,8 @@ class ProductsController < ApplicationController
    # GET /products/1
    # GET /products/1.json
   def show
-    @comments = @product.comments.order("created_at DESC")
+    @comments = @product.comments.all.order("created_at DESC")
+    @comments = @product.comments.paginate(page: params[:page], per_page: 3)
   end
 
    # GET /products/new
